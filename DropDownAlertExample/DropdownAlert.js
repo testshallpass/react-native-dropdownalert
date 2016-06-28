f6cc6e41e082d1b733901a5ec6b4fa64bf4a3833
devbriguy <briansinnicke@gmail.com>
2016-06-27 23:30:14 -0400

    Add .npmignore; Setup npm project structure; Update README to reflect new project structure; Add MIT license;

---
+++ DropDownAlertExample/DropdownAlert.js
@@ -0,0 +1,233 @@
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  Animated,
+  Modal,
+  StatusBar,
+  Image
+} from "react-native";
+
+var closeTimeoutId
+
+var DropdownAlert = React.createClass({
+  getDefaultProps: function() {
+    return {
+      closeInterval: -1,
+      backgroundColor: 'steelblue',
+      imageUri: '',
+      imageSrc: '',
+      textColor: 'white',
+      fontFamily: 'HelveticaNeue',
+      startDelta: -100,
+      endDelta: 0
+    }
+  },
+  getInitialState: function() {
+    return {
+      fadeAnim: new Animated.Value(0),
+      duration: 450,
+      visible: false,
+      type: 'info',
+      message: '',
+      title: '',
+      isOpen: false
+    }
+  },
+  renderTitle: function() {
+    if (this.state.title.length > 0) {
+      var style = styles.title
+      if (this.state.type == 'custom') {
+        style = [styles.title, {color: this.props.textColor, fontFamily: this.props.fontFamily}]
+      }
+      return (
+        <Text style={style} numberOfLines={(this.state.message.length > 0) ? 1 : 3}>
+          {this.state.title}
+        </Text>
+      )
+    }
+    return null
+  },
+  renderMessage: function() {
+    if (this.state.message.length > 0) {
+      var style = styles.message
+      if (this.state.type == 'custom') {
+        style = [styles.message, {color: this.props.textColor, fontFamily: this.props.fontFamily}]
+      }
+      return (
+        <Text style={style} numberOfLines={(this.state.title.length > 0) ? 2 : 3}>
+          {this.state.message}
+        </Text>
+      )
+    }
+    return null
+  },
+  renderImage: function(src) {
+    if (this.state.type == 'custom' && this.props.imageUri.length > 0) {
+      var uri = this.props.imageUri
+      return (
+        <Image style={[styles.image, {width: 36, height: 36}]} source={{uri: uri}} />
+      )
+    } else if (src != null) {
+      return (
+        <Image style={styles.image} source={src} />
+      )
+    } else {
+      return null
+    }
+  },
+  renderDropDown: function() {
+    if (this.state.visible) {
+      var style
+      var source
+      switch (this.state.type) {
+        case 'info':
+          style = styles.infoContainer
+          source = require('./assets/info.png')
+          break;
+        case 'warn':
+          style = styles.warnContainer
+          source = require('./assets/warn.png')
+          break;
+        case 'error':
+          style = styles.errorContainer
+          source = require('./assets/error.png')
+          break;
+        case 'custom':
+          style = [styles.customContainer, {backgroundColor: this.props.backgroundColor}]
+          source = this.props.imageSrc
+        default:
+      }
+
+      return (
+        <Modal transparent={true} onRequestClose={() => this.dismiss}>
+          <Animated.View style={{
+              opacity: this.state.fadeAnim,
+              transform: [{
+                translateY: this.state.fadeAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [this.props.startDelta, this.props.endDelta]
+                }),
+              }],
+            }}>
+            <StatusBar barStyle="light-content" />
+            <TouchableHighlight onPress={this.dismiss} underlayColor={'lightgray'}>
+              <View style={style}>
+                {this.renderImage(source)}
+                <View style={styles.titleMsgContainer}>
+                  {this.renderTitle()}
+                  {this.renderMessage()}
+                </View>
+              </View>
+            </TouchableHighlight>
+          </Animated.View>
+      </Modal>
+      )
+    } else {
+      return (<View />)
+    }
+  },
+  render() {
+    return (
+      this.renderDropDown()
+    )
+  },
+  alert: function(type, title, message) {
+    if (this.state.isOpen) {
+      this.dismiss()
+      return
+    }
+    if (this.state.visible == false) {
+      this.setState({
+        visible: true,
+        type: type,
+        message: message,
+        title: title,
+        isOpen: true
+      })
+    }
+    Animated.timing(
+      this.state.fadeAnim, {
+        toValue: 1,
+        duration: this.state.duration
+      }
+    ).start()
+     if (this.props.closeInterval > 1) {
+      closeTimeoutId = setTimeout(function() {
+        this.dismiss()
+      }.bind(this), this.props.closeInterval)
+    }
+  },
+  dismiss: function() {
+    if (this.state.isOpen) {
+      if (closeTimeoutId != null) {
+        clearTimeout(closeTimeoutId)
+      }
+      Animated.timing(
+        this.state.fadeAnim, {
+          toValue: 0,
+          duration: this.state.duration
+        }
+      ).start()
+      setTimeout(function() {
+        if (this.state.visible) {
+          this.setState({
+            visible: false,
+            isOpen: false
+          })
+        }
+      }.bind(this), (this.state.duration))
+    }
+  },
+})
+
+var styles = StyleSheet.create({
+  infoContainer: {
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'steelblue',
+  },
+  warnContainer: {
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'peru',
+  },
+  errorContainer: {
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#cc3232'
+  },
+  customContainer: {
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleMsgContainer: {
+    flex: 1,
+    padding: 8
+  },
+  image: {
+    padding: 8,
+  },
+  title: {
+    fontSize: 16,
+    textAlign: 'left',
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: 'transparent'
+  },
+  message: {
+    fontSize: 14,
+    textAlign: 'left',
+    fontWeight: 'normal',
+    color: 'white',
+    backgroundColor: 'transparent'
+  },
+})
+module.exports = DropdownAlert
--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 ### react-native-dropdownalert
 
-A rather easy way to display bits of infomation to your user without invading the user interface. Try 1 of 3 types or roll your own custom type (see props below). Tap the alert to close or provide an time interval to close automatically.
+A dropdown alert that allows you to display bits of information to your user. Try 1 of 3 types or roll your own custom type (see props below). Tap the alert to close or provide a time interval to close automatically.
 
 ### Demo
 
-![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/demo.gif)
+![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/screenshots/demo.gif)
 
 
 ### Types 
 
-![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/info.png)
-![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/warning.png)
-![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/error.png)
-![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/custom.png)
+![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/screenshots/info.png)
+![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/screenshots/warning.png)
+![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/screenshots/error.png)
+![screenshot](https://raw.github.com/devBrian/react-native-dropdownalert/master/screenshots/custom.png)
 
 ### Props
 | Props name | Type | Description | Platform | Default
@@ -32,3 +32,25 @@ A rather easy way to display bits of infomation to your user without invading th
 * usage example
 
 Inspired by: https://github.com/cwRichardKim/RKDropdownAlert
+
+MIT License
+
+Copyright (c) 2016 devBrian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
