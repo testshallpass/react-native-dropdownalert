af159fea5814a82bd4a943c9bf3c7cef8842ef09
devbriguy <briansinnicke@gmail.com>
2016-06-29 20:23:41 -0400

    Update DropdownAlert example; Add more keywords to package
    Remove reference to local dropdownalert
    Add dropdownalert module
    Refactor method and styles

--- DropDownAlertExample/Main.js
+++ DropDownAlertExample/Main.js
@@ -1,4 +1,4 @@
-import DropdownAlert from './DropdownAlert'
+import DropdownAlert from 'react-native-dropdownalert'
 import React, { Component } from 'react';
 
 import {
@@ -13,37 +13,37 @@ import {
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.openAlert = this.openAlert.bind(this)
-    this.closeAlert = this.closeAlert.bind(this)
+    this.showAlert = this.showAlert.bind(this)
+    this.dismissAlert = this.dismissAlert.bind(this)
   }
   render() {
     return (
       <View style={{flex: 1}}>
-      <DropdownAlert ref={"dropdownalert"} backgroundColor={'darkcyan'} imageUri={'https://facebook.github.io/react/img/logo_og.png'} fontFamily={'Gill Sans'} closeInterval={5000} />
-      <ScrollView>
-      <Text style={[styles.welcome, {marginTop: 22}]}> {'Dropdown Alerts'}</Text>
-        <TouchableHighlight style={styles.container} onPress={() => this.openAlert('info')} underlayColor={'lightgray'}>
-            <Text style={styles.welcome}>
+      <DropdownAlert ref={"dropdownalert"} backgroundColor={'darkcyan'} imageUri={'https://facebook.github.io/react/img/logo_og.png'} fontFamily={'Gill Sans'} />
+      <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.text}>{'Dropdown Alerts'}</Text>
+        <TouchableHighlight style={styles.button} onPress={() => this.showAlert('info')} underlayColor={'lightgray'}>
+            <Text style={styles.text}>
               {'Info'}
             </Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.container} onPress={() => this.openAlert('warn')} underlayColor={'lightgray'}>
-            <Text style={styles.welcome}>
+        <TouchableHighlight style={styles.button} onPress={() => this.showAlert('warn')} underlayColor={'lightgray'}>
+            <Text style={styles.text}>
               {'Warning'}
             </Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.container} onPress={() => this.openAlert('error')} underlayColor={'lightgray'}>
-            <Text style={styles.welcome}>
+        <TouchableHighlight style={styles.button} onPress={() => this.showAlert('error')} underlayColor={'lightgray'}>
+            <Text style={styles.text}>
               {'Error'}
             </Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.container} onPress={() => this.openAlert('custom')} underlayColor={'lightgray'}>
-            <Text style={styles.welcome}>
+        <TouchableHighlight style={styles.button} onPress={() => this.showAlert('custom')} underlayColor={'lightgray'}>
+            <Text style={styles.text}>
               {'Custom'}
             </Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.container} onPress={this.closeAlert} underlayColor={'lightgray'}>
-            <Text style={styles.welcome}>
+        <TouchableHighlight style={styles.button} onPress={this.dismissAlert} underlayColor={'lightgray'}>
+            <Text style={styles.text}>
               {'Close Alert'}
             </Text>
         </TouchableHighlight>
@@ -51,7 +51,7 @@ class Main extends Component {
       </View>
     );
   }
-  openAlert(type) {
+  showAlert(type) {
     switch (type) {
       case 'info':
         this.refs.dropdownalert.alert(type, 'Info', 'System is going down at midnight tonight. We\'ll notify you when it\'s back up.')
@@ -67,7 +67,7 @@ class Main extends Component {
       break;
     }
   }
-  closeAlert() {
+  dismissAlert() {
     this.refs.dropdownalert.dismiss()
   }
 }
@@ -75,21 +75,17 @@ class Main extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'lightgray',
-    margin: 8
+    marginTop: 20,
   },
-  welcome: {
+  button: {
+    backgroundColor: 'lightgray',
+    margin: 8,
+    padding: 10
+  },
+  text: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
     color: '#333333'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
