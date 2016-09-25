54576ebd309fb56a5219cd30444fbdaab7c08b11
devbriguy <briansinnicke@gmail.com>
2016-09-25 16:28:23 -0400

    Update test script to include mocha setup; Add mocha setup; Update tests;

--- test/DropdownAlert-test.js
+++ test/DropdownAlert-test.js
@@ -5,14 +5,9 @@ const { expect } = require('chai')
 
 import DropdownAlert from '../DropdownAlert'
 import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableHighlight,
-  Animated,
-  Modal,
-  StatusBar,
-  Image
+  View, Text, StyleSheet,
+  TouchableHighlight, Animated, Modal,
+  StatusBar, Image
 } from "react-native"
 
 describe('DropdownAlert', () => {
@@ -21,13 +16,13 @@ describe('DropdownAlert', () => {
     should.exist(wrapper)
   })
   it('should find custom type\'s components', () => {
-    let wrapper = shallow(<DropdownAlert imageUri={'https://facebook.github.io/react/img/logo_og.png'} />)
-    wrapper.instance().alert('custom', 'Title', 'Message')
+    let wrapper = shallow(<DropdownAlert imageSrc={'https://facebook.github.io/react/img/logo_og.png'} showCancel={true}  />)
+    wrapper.instance().alert('Title', 'Message')
     wrapper.update()
     expect(wrapper.find(StatusBar)).to.have.length(0)
     expect(wrapper.find(View)).to.have.length(2)
     expect(wrapper.find(Animated.View)).to.have.length(1)
-    expect(wrapper.find(TouchableHighlight)).to.have.length(1)
+    expect(wrapper.find(TouchableHighlight)).to.have.length(2)
     expect(wrapper.find(Text)).to.have.length(2)
     expect(wrapper.find(Image)).to.have.length(1)
   })
@@ -39,13 +34,13 @@ describe('DropdownAlert', () => {
   })
   it('should alert', () => {
     let wrapper = shallow(<DropdownAlert />)
-    wrapper.instance().alert('custom', 'Title', 'Message')
+    wrapper.instance().alert('Title', 'Message')
     wrapper.update()
     wrapper.instance().should.be.ok
   })
   it('should detect isOpen state change', () => {
-    let wrapper = shallow(<DropdownAlert imageUri={'https://facebook.github.io/react/img/logo_og.png'}  />)
-    wrapper.instance().alert('custom', 'Title', 'Message')
+    let wrapper = shallow(<DropdownAlert imageSrc={'https://facebook.github.io/react/img/logo_og.png'} />)
+    wrapper.instance().alert('Title', 'Message')
     wrapper.update()
     wrapper.setState({ isOpen: false })
     expect(wrapper.find(Animated.View)).to.have.length(0)
