52e8bb6a4842b9f7229ad86ff25acc4b1e99c1b3
David Li <david.minxiao.li@gmail.com>
2017-12-11 17:57:33 -0500

    Add error handling to undefined types

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 module.exports.validateType = function(type) {
-  if (type.length === 0 || type === null) {
+  if (!type || type.length === 0 || type === null) {
     console.warn('Missing DropdownAlert type. Available types: info, warn, error or custom');
     return false;
   }
