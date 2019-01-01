abcedf782682652c1f43f3ed957bacebd72299cc
Brian Sinnicke <briansinnicke@gmail.com>
2018-12-31 22:21:00 -0500

    stricter validation for type

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
-module.exports.validateType = function(type) {
-  if (type.length === 0 || type === null) {
-    console.warn('Missing DropdownAlert type. Available types: info, warn, error or custom');
-    return false;
-  }
-  if (type != 'info' && type != 'warn' && type != 'error' && type != 'custom' && type != 'success') {
-    console.warn('Invalid DropdownAlert type. Available types: info, warn, error, success, or custom');
+const validateType = function(type) {
+  const types = ['info', 'warn', 'error', 'success', 'custom'];
+  if (!type || type.length === 0 || types.includes(type) == false) {
+    console.warn('Invalid DropdownAlert type. Available types: info, warn, error, success or custom');
     return false;
   }
   return true;
+};
+module.exports = {
+  validateType,
 };
