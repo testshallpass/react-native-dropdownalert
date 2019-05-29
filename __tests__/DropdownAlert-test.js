66b8dc68f53a41d12c617828bcc59cbb8bc334a6
Brian Sinnicke <briansinnicke@gmail.com>
2019-05-28 21:50:48 -0400

    revise DropdownAlert test cases to pass

--- __tests__/DropdownAlert-test.js
+++ __tests__/DropdownAlert-test.js
@@ -53,42 +53,29 @@ test('expect state variables to change with replace disabled', () => {
   wrapper.instance()._closeTimeoutId = setTimeout(function() {});
   wrapper.update();
   wrapper.instance().alertWithType('info', 'hello', 'world');
-  expect(wrapper.instance().state.type).toBe('info');
-  expect(wrapper.instance().state.title).toBe('hello');
-  expect(wrapper.instance().state.message).toBe('world');
+  expect(wrapper.instance().alertData.type).toBe('info');
+  expect(wrapper.instance().alertData.title).toBe('hello');
+  expect(wrapper.instance().alertData.message).toBe('world');
   expect(wrapper.instance().state.isOpen).toBeTruthy();
   expect(wrapper.instance().state.topValue).toBe(0);
   expect(wrapper.instance()._closeTimeoutId).toBeDefined();
 });
-test('expect invalid type with state isOpen to be false', () => {
+test('expect unknown alert type to be open', () => {
   const wrapper = shallow(<DropdownAlert imageSrc={imageSrc} />);
   wrapper.instance().alertWithType('random', 'hello', 'world');
-  expect(wrapper.instance().state.isOpen).toBeFalsy();
+  expect(wrapper.instance().state.isOpen).toBeTruthy();
 });
 test('expect non string title and message to converted to string', () => {
   const wrapper = shallow(<DropdownAlert imageSrc={imageSrc} />);
-  wrapper.instance().alertWithType('error', {title: 'hello'}, {message: 'world'});
-  console.log(wrapper.instance().state.title);
-  expect(typeof wrapper.instance().state.title === 'string').toBeTruthy();
-  expect(typeof wrapper.instance().state.message === 'string').toBeTruthy();
+  wrapper.instance().alertWithType('error', { title: 'hello' }, { message: 'world' });
+  expect(typeof wrapper.instance().alertData.title === 'string').toBeTruthy();
+  expect(typeof wrapper.instance().alertData.message === 'string').toBeTruthy();
 });
-test('expect close without action to be animation value 0', () => {
+test('expect close with programmatic action to set isOpen to be false', () => {
   const wrapper = shallow(<DropdownAlert imageSrc={imageSrc} />);
-  wrapper.setState({isOpen: true});
-  wrapper.instance().close();
-  expect(wrapper.instance().state.isOpen).toBeTruthy();
-});
-test('expect close with action to be animation value 0', () => {
-  const wrapper = shallow(<DropdownAlert imageSrc={imageSrc} />);
-  wrapper.instance().close('cancel');
-  expect(wrapper.instance().state.animationValue).toEqual(new reactNative.Animated.Value(0));
-});
-test('expect close directly to set isOpen false', () => {
-  const wrapper = shallow(<DropdownAlert imageSrc={imageSrc} />);
-  wrapper.setState({isOpen: true});
-  wrapper.instance()._closeTimeoutId = setTimeout(function() {});
-  wrapper.update();
-  wrapper.instance().closeDirectly();
-  expect(wrapper.instance().state.isOpen).toBeFalsy();
-  expect(wrapper.instance()._closeTimeoutId).toBeDefined();
+  wrapper.setState({ isOpen: true });
+  wrapper.instance().closeAction('programmatic', () => {
+    expect(wrapper.instance().state.isOpen).toBeFalsy();
+    expect(wrapper.instance().state.topValue).toBe(0);
+  });
 });
