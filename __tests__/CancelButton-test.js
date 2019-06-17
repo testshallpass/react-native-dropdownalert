ed853a99bc6b94e6c6113e97cfc4f07616151897
Brian Sinnicke <briansinnicke@gmail.com>
2019-06-16 20:55:57 -0400

    add test cases CancelButton and Constants

--- __tests__/CancelButton-test.js
+++ __tests__/CancelButton-test.js
@@ -4,8 +4,18 @@ import CancelButton from '../CancelButton';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-test('CancelButton renders', () => {
-  const wrapper = shallow(<CancelButton />);
-  const tree = toJson(wrapper);
-  expect(tree).toMatchSnapshot();
+describe('CancelButton', () => {
+  test('renders', () => {
+    const wrapper = shallow(<CancelButton />);
+    const tree = toJson(wrapper);
+    expect(tree).toMatchSnapshot();
+  });
+  test('test onPress to be defined', () => {
+    const onCancel = () => {
+      console.log('Cancelled');
+    };
+    const wrapper = shallow(<CancelButton onPress={onCancel} />);
+    expect(wrapper.prop('onPress')).toEqual(onCancel);
+    expect(wrapper.props().onPress).toBeDefined();
+  });
 });
