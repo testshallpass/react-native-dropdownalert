0cb8bf92705b9c9cbffc70542cf85226fef86e01
testshallpass <testshallpass@gmail.com>
2023-08-05 16:44:19 -0400

    fix example App test; update example build gradle

--- example/__tests__/App.test.tsx
+++ example/__tests__/App.test.tsx
@@ -12,6 +12,25 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-dropdownalert', () => ({
+  __esModule: true,
+  default: 'DropdownAlert',
+  DropdownAlertColor: {
+    Default: 'black',
+    Info: 'blue',
+    Warn: 'yellow',
+    Success: 'green',
+    Error: 'red',
+  },
+  DropdownAlertType: {
+    Info: 'info',
+    Warn: 'warn',
+    Success: 'success',
+    Error: 'error',
+  },
+}));
+
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const component = renderer.create(<App />);
+  expect(component.toJSON()).toMatchSnapshot();
 });
